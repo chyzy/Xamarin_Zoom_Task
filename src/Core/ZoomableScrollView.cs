4ca74e16a7893da54c9ade82a67ad247078fb4a1
Bartosz Chyży <bartosz.chyzy@icsolutions.pl>
2023-03-22 14:33:23 +0100

    Modify datatemplate

--- src/Core/ZoomableScrollView.cs
+++ src/Core/ZoomableScrollView.cs
@@ -8,5 +8,6 @@ namespace Zoom_Task.Core
     public class ZoomableScrollView : ScrollView
     {
         //If its possible implement pinch/pan logic here. If not it should be implemented in native renderers.
+        //You can base on https://github.com/LuckyDucko/ZoomView.Forms/tree/master/ZoomView.Forms if you wish.
     }
 }
