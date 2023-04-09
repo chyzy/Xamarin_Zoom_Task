f39fb18da4fbbe68a23d1dc6b561b999edd834c3
Javed Kalaniya <javedappdevelopment@gmail.com>
2023-04-09 13:22:15 +0530

    Addes sample page with Zoomable controls

---
+++ src/Core/Controls/ZoomScrollView.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Zoom_Task.Core.Controls
+{
+    public class ZoomScrollView : ScrollView, INotifyPropertyChanged
+    {
+        public delegate void ResetZoomScaleDelegate();
+        public ResetZoomScaleDelegate ResetZoomScale;
+
+        public delegate void SetChildInputTransparentDelegate(bool userInteractionLevel);
+        public SetChildInputTransparentDelegate setChildInputTransparent;
+
+        /// <summary>
+        /// Overridden version, to reset zoom scale so errors dont arise
+        /// </summary>
+        public new bool InputTransparent
+        {
+            get => (bool)GetValue(InputTransparentProperty);
+            set
+            {
+                ResetZoomScale();
+                SetValue(InputTransparentProperty, value);
+            }
+        }
+
+
+        public bool UserInteractionEnabled
+        {
+            get => (bool)GetValue(UserInteractionEnabledProperty);
+            set
+            {
+                SetValue(UserInteractionEnabledProperty, value);
+                setChildInputTransparent(value);
+            }
+        }
+
+        private static void OnUserInteractionPropertyChanged(BindableObject bindable, object oldVal, object newVal)
+        {
+            var zoomview = bindable as ZoomScrollView;
+            bool newValResult;
+            if (bool.TryParse(newVal.ToString(), out newValResult))
+            {
+                zoomview.Content.InputTransparent = newValResult;
+            }
+        }
+
+        public static readonly BindableProperty UserInteractionEnabledProperty =
+        BindableProperty.Create(propertyName: nameof(UserInteractionEnabled),
+          returnType: typeof(bool),
+          declaringType: typeof(ZoomScrollView),
+          defaultValue: true,
+          propertyChanged: OnUserInteractionPropertyChanged
+          );
+
+        public ZoomScrollView()
+        {
+        }
+    }
+}
+
---
+++ src/Core/ZoomableScrollViewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Zoom_Task.Core.ViewModels;
+
+namespace Zoom_Task.Core
+{	
+	public partial class ZoomableScrollViewPage : ContentPage
+	{	
+		public ZoomableScrollViewPage ()
+		{
+			InitializeComponent ();
+            BindingContext = new MainPageViewModel();
+        }
+	}
+}
+
